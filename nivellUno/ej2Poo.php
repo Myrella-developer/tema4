e5677167d60754a1eb94da6c186790cf0b893b00
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-05-14 22:10:00 +0200

    ejercicio 2 concluido

--- nivellUno/ej2Poo.php
+++ nivellUno/ej2Poo.php
@@ -1,5 +1,32 @@
 <?php
     class Shape {
-
+        protected $width;
+        protected $height;
+        public function __construct($width, $height) {
+            $this -> width = $width;
+            $this -> height   = $height;
+        }        
     }
+    
+    class Rectangle extends Shape {
+        public function area() {
+            $areaRectangle =  $this -> width * $this -> height;
+
+            return 'La area de lo Rectangulo es de:  '.$areaRectangle;
+        }
+    }
+    class Triangle extends Shape {
+        public function area() {
+            $areaTriangle = ($this -> width * $this -> height) / 2;
+
+            return 'La area de lo Triangulo es de: '.$areaTriangle;
+        }
+    }
+
+    $rectangle = new Rectangle(6, 12);
+    echo $rectangle -> area();
+
+    $triangle = new Triangle(8, 4);
+    echo $triangle -> area();
+
 ?>
