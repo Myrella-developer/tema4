d9cb31dc6a48e145d31c97f0971a088719ce09e5
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-05-14 21:30:26 +0200

    creaccion de carpeta nivell 1 y ejercicio 1 concluido

---
+++ nivellUno/ej1Poo.php
@@ -0,0 +1,35 @@
+<?php
+   class Employee {
+
+    public $name;
+    public $salary;
+
+    public function myMethod($name, $salary) {
+        $this -> name = $name;
+        $this -> salary = $salary;
+    }
+    public function printTaxes(){
+
+        echo $this -> name;
+        if($this -> salary > 6000){
+            echo " --> ¡Pagar Impuestos!";
+        } else {
+            echo " --> ¡Salario Isento de Impuestos!";
+        }
+    }
+
+   }
+
+   $employee1 = new Employee();
+   $employee1 -> myMethod('Maria', 5000);
+   $employee1 -> printTaxes();
+
+   $employee2 = new Employee();
+   $employee2 -> myMethod('Laya', 7000);
+   $employee2 -> printTaxes();
+   
+   $employee3 = new Employee();
+   $employee3 -> myMethod('Amaya', 4000);
+   $employee3 -> printTaxes();
+   
+?>
